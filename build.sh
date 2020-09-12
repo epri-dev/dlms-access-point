61a0ad1473cc716d58d32bc64aeb5309ba346abc
Ed Beroset <beroset@ieee.org>
2020-09-12 10:10:37 -0400

    Add dashboard build to script.

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ PROJECTNAME=dlms-access-point
 REPO=git@github.com:epri-dev/$PROJECTNAME.git
 "$DOCKERBIN" build --target docserver -t "beroset/$PROJECTNAME-docs" "$REPO"
 "$DOCKERBIN" build --target demo -t "beroset/$PROJECTNAME" "$REPO"
+"$DOCKERBIN" build --target demo -t "beroset/$PROJECTNAME-dashboard" "$REPO"
