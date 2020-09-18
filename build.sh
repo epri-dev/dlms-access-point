b97f459ef7e41a2d1ead071991b1fce7ed3e25cc
Ed Beroset <beroset@ieee.org>
2020-09-18 13:14:35 -0400

    Streamline process of building from git.

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ DOCKERBIN=docker
 PROJECTNAME=dlms-access-point
 REPO=git@github.com:epri-dev/$PROJECTNAME.git
 "$DOCKERBIN" build --target builder -t "beroset/$PROJECTNAME-builder" "$REPO"
-"$DOCKERBIN" build --target docserver -t "beroset/$PROJECTNAME-docs" "$REPO"
-"$DOCKERBIN" build --target demo -t "beroset/$PROJECTNAME" "$REPO"
-"$DOCKERBIN" build --target dashboard -t "beroset/$PROJECTNAME-dashboard" "$REPO"
+"$DOCKERBIN" build --target docserver -t "beroset/$PROJECTNAME-docs" "$REPO#:src/docker/docserver"
+"$DOCKERBIN" build --target demo -t "beroset/$PROJECTNAME" "$REPO#:src/docker/demo"
+"$DOCKERBIN" build --target dashboard -t "beroset/$PROJECTNAME-dashboard" "$REPO#:src/docker/dashboard"
