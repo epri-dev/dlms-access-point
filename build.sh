2cf7b183828ac14bc4911a4082a7af5d2d0060d6
Ed Beroset <beroset@ieee.org>
2020-10-19 09:53:48 -0400

    Adjust for branch main instead of master.

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 DOCKERBIN=docker
 PROJECTNAME=dlms-access-point
-REPO=git@github.com:epri-dev/$PROJECTNAME.git
+REPO=git@github.com:epri-dev/$PROJECTNAME.git#main
 "$DOCKERBIN" build --target builder -t "beroset/$PROJECTNAME-builder" "$REPO"
-"$DOCKERBIN" build --target docserver -t "beroset/$PROJECTNAME-docs" "$REPO#:src/docker/docserver"
-"$DOCKERBIN" build --target demo -t "beroset/$PROJECTNAME" "$REPO#:src/docker/demo"
-"$DOCKERBIN" build --target dashboard -t "beroset/$PROJECTNAME-dashboard" "$REPO#:src/docker/dashboard"
+"$DOCKERBIN" build --target docserver -t "beroset/$PROJECTNAME-docs" "$REPO:src/docker/docserver"
+"$DOCKERBIN" build --target demo -t "beroset/$PROJECTNAME" "$REPO:src/docker/demo"
+"$DOCKERBIN" build --target dashboard -t "beroset/$PROJECTNAME-dashboard" "$REPO:src/docker/dashboard"
