b16a305a2ea90fe09d369db16d3a34068818dd0a
Ed Beroset <beroset@ieee.org>
2020-10-21 15:08:03 -0400

    Fix entrypoint script and installer.

--- src/docker/docker-entrypoint.sh
+++ src/docker/docker-entrypoint.sh
@@ -9,5 +9,5 @@ if [ -z "$2" ]; then
     echo "Error: must supply an IPv6 address to Metersim"
     exit 1
 fi
-/tmp/websocket/taskrunner "$3" &
-exec "/tmp/${program}" "$2" 
+taskrunner "$3" &
+exec "${program}" "$2" 
